5536d83813854256812619dd18d534933e477c2b
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2023-06-17 12:59:19 -0500

    style: `black`

--- spimi.py
+++ spimi.py
@@ -19,8 +19,8 @@ class SPIMI:
         self.index_path = os.path.join("index")
         self.data_path = os.path.join("data")
         self.data = json.load(open(os.path.join(self.data_path, "data.json"), "rb"))
-        self.stopwords = stopwords.words('english') + stopwords.words('spanish')
-        self.stemmer = SnowballStemmer('english')
+        self.stopwords = stopwords.words("english") + stopwords.words("spanish")
+        self.stemmer = SnowballStemmer("english")
 
     def processWord(self, word):
         word = word.lower()
@@ -30,9 +30,9 @@ class SPIMI:
         return word
 
     def processText(self, text):
-        text = re.sub(r'\d+', '', text)
-        text = re.sub(r'[^\w\s]', '', text)
-        text = re.sub(r'\b\w{1,2}\b', '', text)
+        text = re.sub(r"\d+", "", text)
+        text = re.sub(r"[^\w\s]", "", text)
+        text = re.sub(r"\b\w{1,2}\b", "", text)
         tokens = nltk.word_tokenize(text)
         tokens = [self.processWord(word) for word in tokens]
         tokens = [word for word in tokens if word is not None]
@@ -58,17 +58,17 @@ class SPIMI:
                 self.flush_block()
         self.flush_block()
         self.merge_blocks()
-        
-    
+
     def flush_block(self):
         sorted_terms = sorted(self.block_dict_temp.keys())
-        block_filepath = os.path.join(self.temp_dir, "block" + str(len(self.blocks))+ ".pkl")
+        block_filepath = os.path.join(
+            self.temp_dir, "block" + str(len(self.blocks)) + ".pkl"
+        )
         with open(block_filepath, "wb") as block_file:
             pickle.dump(sorted_terms, block_file)
             pickle.dump(self.block_dict_temp, block_file)
         self.blocks.append(block_filepath)
         self.block_dict_temp.clear()
-
 
     def merge_two_blocks(self, terms1, term_dict1, block2):
         merged_block = {}
@@ -79,7 +79,9 @@ class SPIMI:
             j = 0
             while i < len(terms1) and j < len(terms2):
                 if terms1[i] == terms2[j]:
-                    merged_block[terms1[i]] = list(set(term_dict1[terms1[i]] + term_dict2[terms2[j]]))
+                    merged_block[terms1[i]] = list(
+                        set(term_dict1[terms1[i]] + term_dict2[terms2[j]])
+                    )
                     i += 1
                     j += 1
                 elif terms1[i] < terms2[j]:
@@ -94,9 +96,8 @@ class SPIMI:
             while j < len(terms2):
                 merged_block[terms2[j]] = term_dict2[terms2[j]]
                 j += 1
-        #os.remove(block2) 
+        # os.remove(block2)
         return merged_block
-
 
     def merge_blocks(self):
         merged_blocks = {}
@@ -106,29 +107,33 @@ class SPIMI:
                     terms1 = pickle.load(block_file)
                     term_dict1 = pickle.load(block_file)
                     merged_blocks = term_dict1
-                
-                #os.remove(self.blocks[i])
+
+                # os.remove(self.blocks[i])
             else:
-                merged_blocks = self.merge_two_blocks(terms1, term_dict1, self.blocks[i])
+                merged_blocks = self.merge_two_blocks(
+                    terms1, term_dict1, self.blocks[i]
+                )
                 terms1 = list(merged_blocks.keys())
                 term_dict1 = merged_blocks
-            
+
         os.makedirs(self.index_path, exist_ok=True)
 
         with open(self.index_path + "/index.pkl", "wb") as index_file:
             pickle.dump(merged_blocks, index_file)
 
         return merged_blocks
-        
+
     def cosine_similarity(self, query_vector, doc_vector):
-        dot_product = sum(query_vector[i] * doc_vector[i] for i in range(len(query_vector)))
-        query_norm = math.sqrt(sum(value ** 2 for value in query_vector))
-        document_norm = math.sqrt(sum(value ** 2 for value in doc_vector))
+        dot_product = sum(
+            query_vector[i] * doc_vector[i] for i in range(len(query_vector))
+        )
+        query_norm = math.sqrt(sum(value**2 for value in query_vector))
+        document_norm = math.sqrt(sum(value**2 for value in doc_vector))
 
         if (query_norm * document_norm) == 0:
             return 0
         return dot_product / (query_norm * document_norm)
-    
+
     def process_query(self, query, k):
         index = open(self.index_path + "/index.pkl", "rb")
         index = pickle.load(index)
@@ -136,14 +141,14 @@ class SPIMI:
         query_vector = []
         for term in query:
             tf = query.count(term)
-            #comprobar si el termino esta en el indice
+            # comprobar si el termino esta en el indice
             if term not in index:
                 df = 0
             else:
                 df = len(index[term])
 
-            idf = math.log(len(self.data.keys())/(1+df), 4)
-            query_vector.append(tf*idf)
+            idf = math.log(len(self.data.keys()) / (1 + df), 4)
+            query_vector.append(tf * idf)
 
         scores = defaultdict(float)
 
@@ -151,19 +156,14 @@ class SPIMI:
         for term in query:
             for doc_id in index[term]:
                 docs.add(doc_id)
-        
+
         for doc_id in docs:
             doc_vector = []
             for term in query:
                 tf = self.data[doc_id].count(term)
                 df = len(index[term])
-                idf = math.log(len(self.data.keys())/(1+df), 4)
-                doc_vector.append(tf*idf)
+                idf = math.log(len(self.data.keys()) / (1 + df), 4)
+                doc_vector.append(tf * idf)
             scores[doc_id] = self.cosine_similarity(query_vector, doc_vector)
 
         return sorted(scores.items(), key=lambda item: item[1], reverse=True)[:k]
-
-
-
-    
-
