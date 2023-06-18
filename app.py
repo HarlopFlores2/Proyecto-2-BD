b5f077774c21c60b899e3f17d8a5bc96c3cd158e
HarlopFlores2 <130115802+HarlopFlores2@users.noreply.github.com>
2023-06-18 01:46:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for, Response
+from flask import Flask, render_template, request
 from database import connector
 from sqlalchemy import text
 
 import sys
 import os
 import time
+import nltk
 
 from spimi import *
+
+#Comentar si ya esta instalado.
+#-----------
+nltk.download('punkt')
+nltk.download('stopwords')
+#-----------
+
 
 db = connector.Manager()
 engine = db.createEngine()
@@ -14,35 +22,24 @@ engine = db.createEngine()
 spimi = SPIMI(10000000)
 #spimi.build_index()
 
-
 app = Flask(__name__)
-
 
 @app.route('/', methods=['POST', 'GET'])
 def buscador():
     return render_template('buscador.html')
-
 
 @app.route('/resultados', methods=['POST', 'GET'])
 def resultados():
     data = spimi.data
     query = request.args.get('query')
     topK = int(request.args.get('topK'))
-    
+
+    # Resultados de SPIMI
     start_time = time.time()
+    resultados_spimi = spimi.process_query(query, topK)
+    elapsed_time_spimi = time.time() - start_time
 
-    resultadostopK = spimi.process_query(query, topK) 
-    
-    elapsed_time = time.time() - start_time
-    print("Tiempo de ejecucion: %s segundos", elapsed_time)
-    return render_template('resultados.html', resultados=resultadostopK, data=data, elapsed_time=elapsed_time)
-
-
-@app.route('/resultados_sql', methods = ['POST', 'GET'])
-def resultados_sql():
-    query = request.args.get('query')
-    topK = request.args.get('topK')
-
+    # Resultados de PostgreSQL
     db_session = db.getSession(engine)
     sql = text(f'''
     select id, abstract, ts_rank_cd(abstract_idx, query) as rank
@@ -52,13 +49,19 @@ def resultados_sql():
     limit {topK};
     ''')
     start_time = time.time()
-    respuesta = db_session.execute(sql)
-    elapsed_time = time.time() - start_time
+    resultados_sql = db_session.execute(sql)
+    elapsed_time_sql = time.time() - start_time
     db_session.close()
-    
-    return render_template('resultados_sql.html', resultados_sql=respuesta, elapsed_time=elapsed_time)
 
+    # Renderiza la plantilla
+    return render_template(
+        'resultados.html',
+        resultados_spimi=resultados_spimi,
+        resultados_sql=resultados_sql,
+        data=data,
+        elapsed_time_spimi=elapsed_time_spimi,
+        elapsed_time_sql=elapsed_time_sql
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
-
