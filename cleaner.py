de3dd94756a7a206953a4f1e1f17e4b94824a67f
HarlopFlores2 <130115802+HarlopFlores2@users.noreply.github.com>
2023-06-18 21:46:12 -0500

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,22 +1,28 @@
 import os
 import json
 
-def split_filtered_json(input_file_path, output_directory):
+
+def filtered_json(input_file_path, output_directory):
     with open(input_file_path, 'r') as input_file:
         # Crea la carpeta destino si no existe
         os.makedirs(output_directory, exist_ok=True)
-        
-        new_dict = {}
+                
+        json_data = []
         for line in input_file:
+            new_dict = {}
             objeto = json.loads(line)
-            new_dict[objeto["id"]] = objeto["abstract"]
-            
-        output_file_path = os.path.join(output_directory, 'data.json')
-        with open(output_file_path, 'w') as output_file:
-            json.dump(new_dict, output_file)
+            new_dict['id'] = objeto['id']
+            new_dict['abstract'] = objeto['abstract']
+            json_data.append(new_dict)
+        
+        if json_data:
+            output_file_path = os.path.join(output_directory, 'newdata.json')
+            with open(output_file_path, 'w') as output_file:
+                json.dump(json_data, output_file)
 
 
-input_file_path = "./arxiv-metadata-oai-snapshot.json"
-output_directory = "./data"
+input_file_path = "./arxiv.json"
+output_directory = "./data2"
+chunk_size = 200000
 
-split_filtered_json(input_file_path, output_directory)
+filtered_json(input_file_path, output_directory)
